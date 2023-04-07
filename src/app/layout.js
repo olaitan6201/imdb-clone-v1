d4ab00c746342da070bc4fd3d7cd10968f09c3f9
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2023-04-07 21:52:31 +0100

    created the navbar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import './globals.css'
 import Providers from './Providers'
+import Navbar from '@/components/Navbar'
 
 export const metadata = {
   title: 'IMDb Clone',
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
           <Header />
 
           {/* NavBar */}
+          <Navbar />
 
           {/* SearchBox */}
 
